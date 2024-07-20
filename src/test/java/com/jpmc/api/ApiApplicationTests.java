e2604a20715d57091a5d87b3054bf4331f84fa4c
Harsh <itsmehxrsh@gmail.com>
2024-07-20 11:04:18 +0530

    Spring Initializr

---
+++ src/test/java/com/jpmc/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpmc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
